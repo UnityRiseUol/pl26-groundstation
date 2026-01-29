c6dc65475e1d894e5ec3dfe3fce09e91b4cf9722
AdamRocket7 <arrypottahfishy@gmail.com>
2026-01-29 17:46:24 +0000

    Add files via upload

---
+++ MapInterface.py
@@ -0,0 +1,89 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Map Interface</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+    <link
+        rel="stylesheet"
+        href="Link to where the relationship variable is on the internet - FIND"
+    />
+    <script src="Link to source file (i think this is the same link as above)"></script>
+
+
+
+    <style>
+        html, body, #map {
+            height: 100%;
+            margin: 0;
+        }
+    </style>
+
+
+
+
+</head>
+<body>
+
+<div id="map"></div>
+
+<script>
+    // Initialize map
+    var map = L.map('map').setView([28.5, -80.6], 6);
+
+    // OpenStreetMap tiles
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Rocket trajectory (lat, lon)
+    var trajectory = [
+        [28.5623, -80.5774],  //launch site
+        [29.0, -79.5],
+        [30.0, -77.0],
+        [31.5, -74.0],
+        [33.0, -70.0]
+    ];
+
+    // Draw trajectory
+    var path = L.polyline(trajectory, {
+        color: 'red',
+        weight: 3
+    }).addTo(map);
+
+    // Markers
+    L.marker(trajectory[0]).addTo(map).bindPopup("Launch");
+    L.marker(trajectory[trajectory.length - 1]).addTo(map).bindPopup("End Point");
+
+    map.fitBounds(path.getBounds());
+</script>
+
+</body>
+</html>
+"""
+
+
+class RocketMap(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Map interface")
+
+        self.browser = QWebEngineView()
+        self.browser.setHtml(HTML_TEMPLATE)
+
+        self.setCGraph(self.browser)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = RocketMap()
+    window.show()
+    sys.exit(app.exec_())
